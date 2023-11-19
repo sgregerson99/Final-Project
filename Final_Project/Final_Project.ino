932aa528038d482d77e965ff4eac214cee878aa0
sgregerson99 <118472358+sgregerson99@users.noreply.github.com>
2023-11-19 14:57:58 -0700

    Update Final_Project.ino

--- Final_Project/Final_Project.ino
+++ Final_Project/Final_Project.ino
@@ -1,5 +1,11 @@
 #include <DS3231.h>
 #include <Wire.h><br>
+#include <Stepper.h>
+int Photo_Resist_Pin = A0;
+int Photo_Resist_Value;
+const int stepsPerRevolution = 2048;
+Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
+
 void setup() {
   // put your setup code here, to run once:
 
