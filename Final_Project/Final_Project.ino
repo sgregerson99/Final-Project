6e27390af26a748bc0ad33c4cf655d2ab8d8d47f
sgregerson99 <118472358+sgregerson99@users.noreply.github.com>
2023-11-19 15:11:53 -0700

    Update Final_Project.ino

--- Final_Project/Final_Project.ino
+++ Final_Project/Final_Project.ino
@@ -3,44 +3,70 @@
 Servo myservo
 #include <Stepper.h>
 int Photo_Resist_Pin = A0;
+const int buttonPin = 7;
 int Photo_Resist_Value;
 const int stepsPerRevolution = 2048;
 Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
+bool previousState = 1;
+bool currentState = 1;
+bool toggle = 0;
 
 void setup() {
   myStepper.setSpeed(15);
   pinMode(Photo_Resist_Pin, INPUT);
   Serial.begin(9600);
   myservo.attach(9);
+  pinMode(mp3_1, OUTPUT);
+  pinMode(mp3_2, OUTPUT);
   // put your setup code here, to run once:
 
 }
 
 void loop() {
+  if (pressed() == true) {
+   toggle = !toggle;
+ }
   Photo_Resist_Value = analogRead(Photo_Resist_Pin);
   Serial.println(Photo_Resist_Value);
   delay(30);
   if (Photo_Resist_Value > 500) {
-    Serial.println("clockwise");
   myStepper.step(stepsPerRevolution); 
   delay(500);
 
   // step one revolution in the other direction:
-  Serial.println("counterclockwise");
   myStepper.step(-stepsPerRevolution);
   delay(500);
    // Serial.println("No pill detected");
     
     
   }
-  if (digitalRead(buttonPin) == LOW){
+  if (toggle == 1){
     servoangle = 180;
     myservo.write(servoangle);
     digitalWrite(mp3_1, LOW);
     delay(20);
   }
-  // put your main code here, to run repeatedly:
+  else {
+    digitalWrite(mp3_2, LOW);
+  }
+
+  
 
 }
+
+bool pressed() {
+     previousState = currentState;
+    currentState = digitalRead(buttonPin);
+  // read the state of the pushbutton value:
+    if (currentState == LOW && previousState == HIGH) {
+      delay(10);
+      return true;
+  } else {
+      return false;
+  }
+}
+
+
+
 
 
